db1566fa2138364d8e3cfb4472ed12ced4ebdbdb
fuadmoin <fuadamoin@gmail.com>
2023-02-06 19:59:36 +0300

    Fix linter errors

--- portfolio.js
+++ portfolio.js
@@ -1,24 +1,23 @@
-let navigation = document.querySelector('.hidden');
-let close = document.querySelector('#close');
-let option = document.querySelector('#option');
-let cont = document.querySelector('#cont');
-let about = document.querySelector('#about');
-let port = document.querySelector('#port');
+const navigation = document.querySelector('.hidden');
+const close = document.querySelector('#close');
+const option = document.querySelector('#option');
+const cont = document.querySelector('#cont');
+const about = document.querySelector('#about');
+const port = document.querySelector('#port');
 
-option.addEventListener('click', function(event) {
-    navigation.style.display = "flex";
-    });
-   
+option.addEventListener('click', () => {
+  navigation.style.display = 'flex';
+});
 
-    close.addEventListener('click', function(event) {
-        navigation.style.display = 'none';
-        });
-        cont.addEventListener('click', function(event) {
-            navigation.style.display = 'none';
-            });
-            about.addEventListener('click', function(event) {
-                navigation.style.display = 'none';
-                });
-                port.addEventListener('click', function(event) {
-                    navigation.style.display = 'none';
-                    });
+close.addEventListener('click', () => {
+  navigation.style.display = 'none';
+});
+cont.addEventListener('click', () => {
+  navigation.style.display = 'none';
+});
+about.addEventListener('click', () => {
+  navigation.style.display = 'none';
+});
+port.addEventListener('click', () => {
+  navigation.style.display = 'none';
+});
