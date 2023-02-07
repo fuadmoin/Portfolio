3d0eb581c85b67db011d54b1e00e6f5f67df3385
ometman <114116376+ometman@users.noreply.github.com>
2023-02-07 15:38:54 +0000

    Update javascript and html for card popup

--- portfolio.js
+++ portfolio.js
@@ -20,17 +20,193 @@ let rb2= document.querySelector('.rb2');
 let rb6= document.querySelector('.rb6');
 
 
-const cardcollection1 = [
-    card1= {
-
+const cardCollection1 = [
+    {
+          id: '1',
+          title: 'Professional Artm \n Printing Data More',
+          description: "description",
+          featuredimg: ['./images/detailspopupdesk.png', './images/detailspopupmobile.png'],
+          technologies: ['Ruby on rails', 'Css', 'Javascript'],
+          seeproject: 'See Project',
+          seelive: 'See live',
+          source: 'See source'
     },
+
+    {
+        id: '2',
+        title: 'Printing Data More',
+        description: "description",
+        featuredimg: ['./images/detailspopupdesk.png', './images/detailspopupmobile.png'],
+        technologies: ['Ruby on rails', 'Css', 'Javascript'],
+        seeproject: 'See Project',
+        seelive: 'See live',
+        source: 'See source'
+  },
+
+  {
+    id: '3',
+    title: 'Printing Data More',
+    description: "description",
+    featuredimg: ['./images/detailspopupdesk.png', './images/detailspopupmobile.png'],
+    technologies: ['Ruby on rails', 'Css', 'Javascript'],
+    seeproject: 'See Project',
+    seelive: 'See live',
+    source: 'See source'
+},
+
+{
+    id: '4',
+    title: 'Professional Data Art Printing Data More',
+    description: "description",
+    featuredimg: ['./images/detailspopupdesk.png', './images/detailspopupmobile.png'],
+    technologies: ['Ruby on rails', 'Css', 'Javascript'],
+    seeproject: 'See Project',
+    seelive: 'See live',
+    source: 'See source'
+},
+
+{
+    id: '5',
+    title: 'Printing Data More',
+    description: "description",
+    featuredimg: ['./images/detailspopupdesk.png', './images/detailspopupmobile.png'],
+    technologies: ['Ruby on rails', 'Css', 'Javascript'],
+    seeproject: 'See Project',
+    seelive: 'See live',
+    source: 'See source'
+},
+
+{
+    id: '6',
+    title: 'Printing Data More',
+    description: "description",
+    featuredimg: ['./images/detailspopupdesk.png', './images/detailspopupmobile.png'],
+    technologies: ['Ruby on rails', 'Css', 'Javascript'],
+    seeproject: 'See Project',
+    seelive: 'See live',
+    source: 'See source'
+}
+
     
-    card2: document.querySelector('.card2'),
-    card3: document.querySelector('.card3'),
+    
+
 ]
 
 
+const mobileCards = document.getElementById('mobile-card-work')
+mobileCards.classList.add('card-work');
 
+for(let card in cardCollection1) {
+    mobileCards.innerHTML = `
+                <section class=" cardworks cardwork-one">
+                    <div class="action">
+                        <button type="button" class="btn">See Project</button> 
+                    </div>
+                    <article class="cardwork-one-rb">
+                        <p class="cwo-title-post">${cardCollection1[card].title}</p>
+                        <p class="cwo-supporting-text">A daily selection of privately <br>
+                            personalized reads; no accounts or <br>
+                            sign-ups required. has been the <br>
+                            industry's standard</p>
+                        <ul class="cwo-tags">
+                            <li class="cw-li cw-html"><small class="cw-text">html</small></li>
+                            <li class="cw-li cw-bootstrap"><small class="cw-text">bootstrap</small></li>
+                            <li class="cw-li cw-ruby"><small class="cw-text">Ruby</small></li>
+                        </ul>
+                    </article>
+                    <div class="mask-group"></div>
+                </section>
+                <section class=" cardworks cardwork-two">
+                    <div class="action">
+                        <button type="button" class="btn">See Project</button>                       
+                    </div>
+                    <article class="cardwork-one-rb">
+                        <p class="cwo-title-post">Profesional Art <br>Printing Data</p>
+                        <p class="cwo-supporting-text">A daily selection of privately <br>
+                            personalized reads; no accounts or <br>
+                            sign-ups required. has been the <br>
+                            industry's standard</p>
+                        <ul class="cwo-tags">
+                            <li class="cw-li cw-html"><small class="cw-text">html</small></li>
+                            <li class="cw-li cw-bootstrap"><small class="cw-text">bootstrap</small></li>
+                            <li class="cw-li cw-ruby"><small class="cw-text">Ruby</small></li>
+                        </ul>
+                    </article>
+                    <div class="mask-group"></div>
+                </section>
+                <section class=" cardworks cardwork-three">
+                    <div class="action">
+                        <button type="button" class="btn">See Project</button>                       
+                    </div>
+                    <article class="cardwork-one-rb">
+                        <p class="cwo-title-post">Profesional Art <br>Printing Data</p>
+                        <p class="cwo-supporting-text">A daily selection of privately <br>
+                            personalized reads; no accounts or <br>
+                            sign-ups required. has been the <br>
+                            industry's standard</p>
+                        <ul class="cwo-tags">
+                            <li class="cw-li cw-html"><small class="cw-text">html</small></li>
+                            <li class="cw-li cw-bootstrap"><small class="cw-text">bootstrap</small></li>
+                            <li class="cw-li cw-ruby"><small class="cw-text">Ruby</small></li>
+                        </ul>
+                    </article>
+                    <div class="mask-group"></div>
+                </section>
+                <section class=" cardworks cardwork-four">
+                    <div class="action">
+                        <button type="button" class="btn">See Project</button>                      
+                    </div>
+                    <article class="cardwork-one-rb">
+                        <p class="cwo-title-post">Profesional Art <br>Printing Data</p>
+                        <p class="cwo-supporting-text">A daily selection of privately <br>
+                            personalized reads; no accounts or <br>
+                            sign-ups required. has been the <br>
+                            industry's standard</p>
+                        <ul class="cwo-tags">
+                            <li class="cw-li cw-html"><small class="cw-text">html</small></li>
+                            <li class="cw-li cw-bootstrap"><small class="cw-text">bootstrap</small></li>
+                            <li class="cw-li cw-ruby"><small class="cw-text">Ruby</small></li>
+                        </ul>
+                    </article>
+                    <div class="mask-group"></div>
+                </section>
+                <section class=" cardworks cardwork-five">
+                    <div class="action">
+                        <button type="button" class="btn">See Project</button>                       
+                    </div>
+                    <article class="cardwork-one-rb">
+                        <p class="cwo-title-post">Profesional Art <br>Printing Data</p>
+                        <p class="cwo-supporting-text">A daily selection of privately <br>
+                            personalized reads; no accounts or <br>
+                            sign-ups required. has been the <br>
+                            industry's standard</p>
+                        <ul class="cwo-tags">
+                            <li class="cw-li cw-html"><small class="cw-text">html</small></li>
+                            <li class="cw-li cw-bootstrap"><small class="cw-text">bootstrap</small></li>
+                            <li class="cw-li cw-ruby"><small class="cw-text">Ruby</small></li>
+                        </ul>
+                    </article>
+                    <div class="mask-group"></div>
+                </section>
+                <section class=" cardworks cardwork-six">
+                    <div class="action">
+                        <button type="button" class="btn">See Project</button>                       
+                    </div>
+                    <article class="cardwork-one-rb">
+                        <p class="cwo-title-post">Profesional Art <br>Printing Data</p>
+                        <p class="cwo-supporting-text">A daily selection of privately <br>
+                            personalized reads; no accounts or <br>
+                            sign-ups required. has been the <br>
+                            industry's standard</p>
+                        <ul class="cwo-tags">
+                            <li class="cw-li cw-html"><small class="cw-text">html</small></li>
+                            <li class="cw-li cw-bootstrap"><small class="cw-text">bootstrap</small></li>
+                            <li class="cw-li cw-ruby"><small class="cw-text">Ruby</small></li>
+                        </ul>
+                    </article>
+                    <div class="mask-group"></div>
+                </section> `
+}
 
 
 
